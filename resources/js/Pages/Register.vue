4d8860dc04424bec64928a0c249861db876e80e7
RasuliNaurizbaev <rasuli.naurizbaev@gmail.com>
2025-04-11 17:30:42 +0900

    ""

--- resources/js/Pages/Register.vue
+++ resources/js/Pages/Register.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid class="fill-height">
-    <v-row justify="center" align="center">
+    <v-row justify="center">
       <v-col cols="12" sm="8" md="4">
         <v-card class="pa-4 elevation-3">
           <!-- Card Header -->
